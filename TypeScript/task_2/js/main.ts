5ca0ac4ebb3ba76b920ae0629c3ad6f604b262eb
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-05-22 15:45:01 +0200

    feat: Created interface Directors that extends from Teacher and require numberOfReports (number)

---
+++ TypeScript/task_2/js/main.ts
@@ -0,0 +1,21 @@
+interface Teacher {
+  [index: string]: any;
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
